ef5c90e6cfc2b4ec0d5675ed23dfd9d6b06a1c34
mikelovelace <miketlovelace@gmail.com>
2022-11-26 22:44:45 -0500

    added notes

--- index.js
+++ index.js
@@ -2,6 +2,128 @@
 /* ❗❗ NOTE: PLEASE USE INDIVIDUAL KEYS FOR YOUR CONSTRUCTOR PARAMETERS, NOT OBJECTS. THE TESTS WILL NOT PASS WITH OBJECTS. ❗❗  */
 
 /*
+=========Window Binding=========
+The 'this' keyword defaults to the window object unless you are in strict mode ('use strict') in which case it will return undefined.
+
+example:
+cat = "meow" -> window object will return "meow" - javascript automatically declares the variable because use strict is not on
+
+example:
+'use strict';
+cat = meow -> undefined - javascript does not try and correct the code because it is in strict mode
+
+============================================================================================================================================
+
+
+
+
+
+=========Implicit Binding=========
+The 'this' keyword applies to objects with methods and refers to whatever is left of the dot when the function is invoked.
+'this' has no meaning until the function is invoked
+
+example:
+const myCat = {
+  name: "abby",
+  meow: "meeeoow",
+  cat: function() {
+    console.log(this.meow) // this refers to whatever is to the left of the dot when the function is invoked below, In this case it is myCat
+  }
+}
+
+myCat.cat()
+============================================================================================================================================
+
+
+
+
+
+=========Explicit Binding=========
+'this' refers to whatever we explicitly tell javascript to bind it to using .call, .apply, or .bind.
+.call - will immediatly invoke the function -- we pass in arguments 1 by 1
+.apply - also immediatly invokes the function -- we pass in arguments as an array
+.bind - does NOT immediatly invoke the function -- we pass in aguments 1 by 1. -- Returns a brand new function that can be invoked later
+============================================================================================================================================
+
+example:
+
+function pet() {
+  console.log(this.name)
+} 
+
+const myCat = {
+  name: "abby",
+  age: 17
+}
+
+const myDog = {
+  name: "nugan",
+  age: 18
+}
+
+.call -- pet.call(myCat) // Supply .call with the argument of what we want to assign 'this' to
+
+.bind -- const newPet = pet.bind(myCat) // Creates new function and binds myCat to it
+
+.apply() = Borrow an object's functions. Similar to call(), but pass an array in place of extra individual arguments
+
+let classRoom = {
+
+    takeAttendence: function(students){
+        console.log(this.name,"is taking attendence");
+        for(let i in students){
+            console.log(students[i]);
+        }
+    }
+}
+
+let teacher = {
+    name: "Mrs.Puff"
+}
+
+let students = ["Spongebob","Patrick","Sandy"];
+students.push("Squidward");
+classRoom.takeAttendence.apply(teacher, [students]);
+
+
+
+
+=========New Binding=========
+'this' refers to a newly created object invoked as a constructor function
+
+example:
+// passing in all attribs 1 by 1
+
+function Food(favorite) {
+  this.food = favorite;
+}
+
+const myFavFood = new Food("Mexican")
+console.log(`My favorite food is ${MyFavFood.food}) // 'this' refers to the food key in the constructor function Food
+
+
+
+example:
+// passing in 1 argument
+
+function Food(attr) {
+  this.food = attr.food;
+}
+
+const myFavFood = new Food({ // an object
+  food: "Mexican"
+})
+
+console.log(`My favorite food is ${myFavFood.food})
+
+============================================================================================================================================
+*/
+
+
+
+
+
+/*
   TASK 1
     - Write a Person Constructor that initializes `name` and `age` from arguments.
     - All instances of Person should initialize with an empty `stomach` array.
